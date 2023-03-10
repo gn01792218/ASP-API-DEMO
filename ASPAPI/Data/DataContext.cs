0accd03ffa0fdf03f92e675c81a44d9db058301d
gn01792218 <gn01792218@hotmail.com>
2023-03-10 09:48:02 +0800

    DI DbContext and Connect to DB

---
+++ ASPAPI/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using ASPAPI.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPAPI.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        { 
+        }
+
+        //建立table
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
--- ASPAPI/Models/Domain/WalkDifficulty.cs
+++ ASPAPI/Models/Domain/WalkDifficulty.cs
@@ -2,5 +2,8 @@
 {
     public class WalkDifficulty
     {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+
     }
 }
